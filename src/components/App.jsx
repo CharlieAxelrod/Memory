f9a764bc209e15bba0873d192707ca627bcfda71
Charlie Axelrod <earwaxelrod@Charlies-MacBook-Air.local>
2019-03-09 14:21:17 -0800

    Sets up React skeleton

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render(){
+    return <div>HI</div>
+  }
+}
+
+ReactDOM.render(< App />, document.getElementById('app'));
